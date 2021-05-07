63aceaee9215ba39428103efb98dda6e2d28f6b5
Jose Luis Martinez <pplusdomain@gmail.com>
2021-05-07 11:14:09 +0200

    Bump version to 0.15 for next release

--- lib/Cfn.pm
+++ lib/Cfn.pm
@@ -904,7 +904,7 @@ package Cfn {
   has Description => (isa => 'Str', is => 'rw');
   has Transform => (isa => 'Cfn::Transform', is => 'rw', coerce => 1);
 
-  our $VERSION = '0.14';
+  our $VERSION = '0.15';
 
   has Parameters => (
     is => 'rw',
